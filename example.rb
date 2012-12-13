0bcd707ff6b721a17a788e28e3235733982261f4
Mick Pollard <aussielunix@gmail.com>
2012-12-13 21:33:20 +1100

    got linking working

--- example.rb
+++ example.rb
@@ -9,15 +9,7 @@ ticket_id = '1318622'
 parent_id = '1258480'
 
 rt = RT_Client.new
-rt.add_link(:id => "ticket/#{ticket_id}", :MemberOf => '1258480')
 
+rt.add_link(:id => ticket_id, :MemberOf => 'fsck.com-rt://bulletproof.net.au/ticket/1258480')
 links = rt.links(:id => ticket_id)
-
 pp links
-
-=begin
-----
-{:DependsOn=>"1258480", :id=>"1318622"}
-----
-"DependsOn: 1258480\nid: 1318622"
-=end
