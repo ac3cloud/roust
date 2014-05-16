8af8b8dcdb37e93d5eb8054f43fd9883cad7925f
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-05-16 12:17:39 +1000

    Add lookup method for queues

--- lib/roust.rb
+++ lib/roust.rb
@@ -264,6 +264,25 @@ class Roust
     end
   end
 
+  # id can be numeric (e.g. 28) or textual (e.g. sales)
+  def queue(id)
+    response = self.class.get("/queue/#{id}")
+
+    body, status = handle_response(response)
+    case body
+    when /No queue named/
+      nil
+    else
+      body.gsub!(/\n\s*\n/,"\n") # remove blank lines for Mail
+      message = Mail.new(body)
+      Hash[message.header.fields.map {|header|
+        key   = header.name.to_s.downcase
+        value = header.value.to_s
+        [ key, value ]
+      }]
+    end
+  end
+
   private
   def handle_response(response)
     body   = response.body
--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -41,6 +41,16 @@ describe "Roust" do
        to_return(:status  => 200,
                  :body    => mocks_path.join('user-dan@us.example.txt').read,
                  :headers => {})
+
+    stub_request(:get, "http://rt.example.org/REST/1.0/queue/13").
+       to_return(:status => 200,
+                 :body    => mocks_path.join('queue-13.txt').read,
+                 :headers => {})
+
+    stub_request(:get, "http://rt.example.org/REST/1.0/queue/nil").
+       to_return(:status => 200,
+                 :body    => mocks_path.join('queue-nil.txt').read,
+                 :headers => {})
   end
 
   it "authenticates on instantiation" do
@@ -110,7 +120,7 @@ describe "Roust" do
     end
   end
 
-  it "can find user details" do
+  it "can lookup user details" do
     rt = Roust.new(@credentials)
     rt.authenticated?.should be_true
 
@@ -119,6 +129,30 @@ describe "Roust" do
     user = rt.user("dan@us.example")
     attrs.each do |attr|
       user[attr].should_not be_nil, "#{attr} key doesn't exist"
+    end
+  end
+
+  describe 'queue' do
+    it "can lookup queue details" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
+
+      attrs = %w(id name description correspondaddress commentaddress) +
+              %w(initialpriority finalpriority defaultduein)
+
+      queue = rt.queue('13')
+      attrs.each do |attr|
+        queue[attr].should_not be_nil, "#{attr} key doesn't exist"
+      end
+
+    end
+
+    it 'returns nil for unknown queues' do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
+
+      queue = rt.queue('nil')
+      queue.should be_nil
     end
   end
 end
