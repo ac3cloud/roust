99fde9fad44a3bc6b771c44f5cdf51da109f4858
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-05-16 12:27:53 +1000

    Group tests. Test for unknown users.

--- spec/roust_spec.rb
+++ spec/roust_spec.rb
@@ -42,6 +42,11 @@ describe "Roust" do
                  :body    => mocks_path.join('user-dan@us.example.txt').read,
                  :headers => {})
 
+    stub_request(:get, "http://rt.example.org/REST/1.0/user/nil").
+       to_return(:status  => 200,
+                 :body    => mocks_path.join('user-nil.txt').read,
+                 :headers => {})
+
     stub_request(:get, "http://rt.example.org/REST/1.0/queue/13").
        to_return(:status => 200,
                  :body    => mocks_path.join('queue-13.txt').read,
@@ -53,82 +58,96 @@ describe "Roust" do
                  :headers => {})
   end
 
-  it "authenticates on instantiation" do
-    rt = Roust.new(@credentials)
-    rt.authenticated?.should be_true
-  end
-
-  it "can list tickets matching a query" do
-    rt = Roust.new(@credentials)
-    rt.authenticated?.should be_true
-
-    results = rt.search(:query => "id = 1 or id = 2")
-    results.size.should == 2
-    results.each do |result|
-      result.size.should == 2
+  describe 'authentication' do
+    it "authenticates on instantiation" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
     end
   end
 
-  it "can fetch metadata on individual tickets" do
-    rt = Roust.new(@credentials)
-    rt.authenticated?.should be_true
+  describe 'tickets' do
+    it "can list tickets matching a query" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
 
-    ticket = rt.show("1")
-    ticket.should_not be_nil
-
-    attrs = %w(id Subject Queue) +
-            %w(Requestors Cc AdminCc Owner Creator) +
-            %w(Resolved Status) +
-            %w(Starts Started TimeLeft Due TimeWorked TimeEstimated) +
-            %w(LastUpdated Created Told) +
-            %w(Priority FinalPriority InitialPriority)
-
-    attrs.each do |attr|
-      ticket[attr].should_not be_nil, "#{attr} key doesn't exist"
+      results = rt.search(:query => "id = 1 or id = 2")
+      results.size.should == 2
+      results.each do |result|
+        result.size.should == 2
+      end
     end
 
-    %w(Requestors Cc AdminCc).each do |field|
-      ticket[field].size.should > 1
-    end
-  end
+    it "can fetch metadata on individual tickets" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
 
-  it "can fetch transactions on individual tickets" do
-    rt = Roust.new(@credentials)
-    rt.authenticated?.should be_true
+      ticket = rt.show("1")
+      ticket.should_not be_nil
 
-    short = rt.history("1", :format => "short")
+      attrs = %w(id Subject Queue) +
+              %w(Requestors Cc AdminCc Owner Creator) +
+              %w(Resolved Status) +
+              %w(Starts Started TimeLeft Due TimeWorked TimeEstimated) +
+              %w(LastUpdated Created Told) +
+              %w(Priority FinalPriority InitialPriority)
 
-    short.size.should > 1
-    short.each do |txn|
-      txn.size.should == 2
-      txn.first.should match(/^\d+$/)
-      txn.last.should  match(/^\w.*\w$/)
-    end
-
-    #attrs = %w(ticket data oldvalue creator timetaken) +
-    #        %w(id type field newvalue content description) +
-    #        %w(attachments created)
-    attrs = %w(ticket data oldvalue timetaken) +
-            %w(id type field newvalue content description)
-
-    long = rt.history("1", :format => "long")
-    long.size.should > 0
-    long.each do |txn|
       attrs.each do |attr|
-        txn[attr].should_not be_nil, "#{attr} key doesn't exist"
+        ticket[attr].should_not be_nil, "#{attr} key doesn't exist"
+      end
+
+      %w(Requestors Cc AdminCc).each do |field|
+        ticket[field].size.should > 1
+      end
+    end
+
+    it "can fetch transactions on individual tickets" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
+
+      short = rt.history("1", :format => "short")
+
+      short.size.should > 1
+      short.each do |txn|
+        txn.size.should == 2
+        txn.first.should match(/^\d+$/)
+        txn.last.should  match(/^\w.*\w$/)
+      end
+
+      #attrs = %w(ticket data oldvalue creator timetaken) +
+      #        %w(id type field newvalue content description) +
+      #        %w(attachments created)
+      attrs = %w(ticket data oldvalue timetaken) +
+              %w(id type field newvalue content description)
+
+      long = rt.history("1", :format => "long")
+      long.size.should > 0
+      long.each do |txn|
+        attrs.each do |attr|
+          txn[attr].should_not be_nil, "#{attr} key doesn't exist"
+        end
       end
     end
   end
 
-  it "can lookup user details" do
-    rt = Roust.new(@credentials)
-    rt.authenticated?.should be_true
+  describe 'user' do
+    it "can lookup user details" do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
 
-    attrs = %w(name realname gecos nickname emailaddress id lang password)
+      attrs = %w(name realname gecos nickname emailaddress id lang password)
 
-    user = rt.user("dan@us.example")
-    attrs.each do |attr|
-      user[attr].should_not be_nil, "#{attr} key doesn't exist"
+      user = rt.user("dan@us.example")
+      attrs.each do |attr|
+        user[attr].should_not be_nil, "#{attr} key doesn't exist"
+      end
+    end
+
+    it 'returns nil for unknown users' do
+      rt = Roust.new(@credentials)
+      rt.authenticated?.should be_true
+
+      queue = rt.user('nil')
+      queue.should be_nil
     end
   end
 
