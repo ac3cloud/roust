dd7f89d667c27870ff33bc2c1bc098dd8cc7f179
Lindsay Holmwood <lindsay@holmwood.id.au>
2014-07-05 22:43:20 +1000

    Don't use short form module declaration. Re-indent

--- lib/roust/queue.rb
+++ lib/roust/queue.rb
@@ -1,21 +1,23 @@
-module Roust::Queue
-  # id can be numeric (e.g. 28) or textual (e.g. sales)
-  def queue_show(id)
-    response = self.class.get("/queue/#{id}")
+class Roust
+  module Queue
+    # id can be numeric (e.g. 28) or textual (e.g. sales)
+    def queue_show(id)
+      response = self.class.get("/queue/#{id}")
 
-    body, _ = explode_response(response)
-    if body =~ /No queue named/
-      nil
-    else
-      body.gsub!(/\n\s*\n/, "\n") # remove blank lines for Mail
-      message = Mail.new(body)
-      Hash[message.header.fields.map { |header|
-        key   = header.name.to_s.downcase
-        value = header.value.to_s
-        [ key, value ]
-      }]
+      body, _ = explode_response(response)
+      if body =~ /No queue named/
+        nil
+      else
+        body.gsub!(/\n\s*\n/, "\n") # remove blank lines for Mail
+        message = Mail.new(body)
+        Hash[message.header.fields.map { |header|
+          key   = header.name.to_s.downcase
+          value = header.value.to_s
+          [ key, value ]
+        }]
+      end
     end
-  end
 
-  alias_method :queue, :queue_show
+    alias_method :queue, :queue_show
+  end
 end
