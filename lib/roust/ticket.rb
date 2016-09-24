de4c6212928481fa4fb03df2ffa065390da0c59c
Dirk Engling <erdgeist@erdgeist.org>
2016-09-24 13:24:46 +0200

    rt44 replies with an 'Correspondence added' body, so roust should not throw

--- lib/roust/ticket.rb
+++ lib/roust/ticket.rb
@@ -100,7 +100,7 @@ class Roust
         body, _ = explode_response(response)
 
         case body
-        when /^# (Message recorded|Comments added)/
+        when /^# (Message recorded|Comments added|Correspondence added)/
           ticket_show(id)
         when /^# You are not allowed to modify ticket \d+/
           raise Unauthorized, body
